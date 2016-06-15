11c4052e61937a6784a74e5a169516d32ada8a0d
felicia.halim <felicia.halim@live.com>
2016-06-15 11:44:41 +0800

    Remove one else if when the Solver::Unknown because the treatment can be covered with other branch

--- lib/Core/SpecialFunctionHandler.cpp
+++ lib/Core/SpecialFunctionHandler.cpp
@@ -436,12 +436,10 @@ SpecialFunctionHandler::handleAbstract(ExecutionState &state,
   bool success = executor.checkImplication(state, e, result);
   assert(success && "FIXME: Unhandled solver failure");
 
-  if (result == Solver::True) {
-    executor.replaceConstraint(state, e);
-  } else if (result == Solver::Unknown) {
-    executor.addConstraint(state, e);
-  } else {
+  if (result == Solver::False) {
     executor.terminateStateOnError(state, "abstraction error", Executor::User);
+  } else {
+    executor.replaceConstraint(state, e);
   }
 }
 
