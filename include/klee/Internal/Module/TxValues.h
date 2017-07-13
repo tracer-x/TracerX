d7d2baa17525c49e14b8e30180f5f91a033d94df
Andrew Santosa <dcsandr@nus.edu.sg>
2017-07-13 12:21:34 +0800

    Swapped the locations for the declarations of AllocationContext and AllocationInfo

--- include/klee/Internal/Module/TxValues.h
+++ include/klee/Internal/Module/TxValues.h
@@ -42,63 +42,6 @@ class TxStateAddress;
 
 class TxStateValue;
 
-class AllocationInfo {
-public:
-  unsigned refCount;
-
-private:
-  ref<Expr> base;
-
-  uint64_t size;
-
-  AllocationInfo(ref<Expr> _base, uint64_t _size)
-      : refCount(0), base(_base), size(_size) {}
-
-public:
-  ~AllocationInfo() {}
-
-  static ref<AllocationInfo> create(ref<Expr> base, uint64_t size) {
-    return ref<AllocationInfo>(new AllocationInfo(base, size));
-  }
-
-  ref<Expr> getBase() { return base; }
-
-  uint64_t getSize() { return size; }
-
-  /// \brief Translate this allocation info into another.
-  ///
-  /// \param other The other translation info to translate to
-  /// \param table The translation table. This table gets updated if this is a
-  /// new translation.
-  ///
-  /// \return true if the translation was successful, false otherwise.
-  bool
-  translate(ref<AllocationInfo> other,
-            std::map<ref<AllocationInfo>, ref<AllocationInfo> > &table) const;
-
-  int compare(const AllocationInfo &other) const;
-
-  /// \brief Print the content of the object to the LLVM error stream
-  void dump() const {
-    print(llvm::errs());
-    llvm::errs() << "\n";
-  }
-
-  /// \brief Print the content of the object into a stream.
-  ///
-  /// \param stream The stream to print the data to.
-  void print(llvm::raw_ostream &stream) const {
-    std::string emptyString;
-    print(stream, emptyString);
-  }
-
-  /// \brief Print the content of the object into a stream.
-  ///
-  /// \param stream The stream to print the data to.
-  /// \param prefix Padding spaces to print before the actual data.
-  void print(llvm::raw_ostream &stream, const std::string &prefix) const;
-};
-
 class AllocationContext {
 
 public:
@@ -173,6 +116,63 @@ public:
     }
     return 3;
   }
+
+  /// \brief Print the content of the object to the LLVM error stream
+  void dump() const {
+    print(llvm::errs());
+    llvm::errs() << "\n";
+  }
+
+  /// \brief Print the content of the object into a stream.
+  ///
+  /// \param stream The stream to print the data to.
+  void print(llvm::raw_ostream &stream) const {
+    std::string emptyString;
+    print(stream, emptyString);
+  }
+
+  /// \brief Print the content of the object into a stream.
+  ///
+  /// \param stream The stream to print the data to.
+  /// \param prefix Padding spaces to print before the actual data.
+  void print(llvm::raw_ostream &stream, const std::string &prefix) const;
+};
+
+class AllocationInfo {
+public:
+  unsigned refCount;
+
+private:
+  ref<Expr> base;
+
+  uint64_t size;
+
+  AllocationInfo(ref<Expr> _base, uint64_t _size)
+      : refCount(0), base(_base), size(_size) {}
+
+public:
+  ~AllocationInfo() {}
+
+  static ref<AllocationInfo> create(ref<Expr> base, uint64_t size) {
+    return ref<AllocationInfo>(new AllocationInfo(base, size));
+  }
+
+  ref<Expr> getBase() { return base; }
+
+  uint64_t getSize() { return size; }
+
+  /// \brief Translate this allocation info into another.
+  ///
+  /// \param other The other translation info to translate to
+  /// \param table The translation table. This table gets updated if this is a
+  /// new translation.
+  ///
+  /// \return true if the translation was successful, false otherwise.
+  bool
+  translate(ref<AllocationInfo> other,
+            std::map<ref<AllocationInfo>, ref<AllocationInfo> > &table) const;
+
+  int compare(const AllocationInfo &other) const;
 
   /// \brief Print the content of the object to the LLVM error stream
   void dump() const {
