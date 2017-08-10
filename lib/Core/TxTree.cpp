27f1f68cecd4c4f4589d91dd7237beb0483bcb4d
rasoolmaghareh <rasool@comp.nus.edu.sg>
2017-08-10 18:50:38 +0800

    Adding wp class

--- lib/Core/TxTree.cpp
+++ lib/Core/TxTree.cpp
@@ -16,6 +16,7 @@
 #include "TxTree.h"
 
 #include "TimingSolver.h"
+#include "WP.h"
 
 #include <klee/CommandLine.h>
 #include <klee/Expr.h>
@@ -2289,61 +2290,11 @@ ref<Expr> TxTreeNode::getInterpolant(
 ref<Expr> TxTreeNode::getWPInterpolant() {
   TimerStatIncrementer t(getWPInterpolantTime);
 
+  WeakestPreCondition *wp = new WeakestPreCondition();
   // Preprocessing phase: marking the instructions that contribute
   // to the target or an infeasible path.
-  std::set<llvm::Value *> markedVariables;
-  for (std::map<KInstruction *, bool>::const_reverse_iterator
-           it = reverseInstructionList.rbegin(),
-           ie = reverseInstructionList.rend();
-       it != ie; ++it) {
-
-    KInstruction *i = (*it).first;
-
-    if (markedVariables.find(i->inst) != markedVariables.end()) {
-      reverseInstructionList[i] = true;
-    } else if (dyn_cast<llvm::StoreInst>(i->inst) &&
-               markedVariables.find(i->inst->getOperand(1)) !=
-                   markedVariables.end()) {
-      reverseInstructionList[i] = true;
-    }
-
-    if ((*it).second == true) {
-      if (dyn_cast<llvm::BinaryOperator>(i->inst) ||
-          dyn_cast<llvm::UnaryInstruction>(i->inst) ||
-          dyn_cast<llvm::StoreInst>(i->inst) ||
-          dyn_cast<llvm::CmpInst>(i->inst)) {
-        unsigned int j = 0;
-        while (j < i->inst->getNumOperands()) {
-          llvm::Value *operand = i->inst->getOperand(j);
-          if (markedVariables.find(operand) == markedVariables.end() &&
-              !dyn_cast<llvm::Constant>(operand)) {
-            markedVariables.insert(operand);
-          }
-          j++;
-        }
-      } else {
-        switch (i->inst->getOpcode()) {
-        case llvm::Instruction::Br: {
-          llvm::BranchInst *bi = cast<llvm::BranchInst>(i->inst);
-          if (bi->isUnconditional()) {
-            klee_error("Unconditional BR should not be marked in WP!");
-          } else {
-            markedVariables.insert(bi->getCondition());
-          }
-          break;
-        }
-        case llvm::Instruction::Call: {
-          // TODO: Add interprocedural marking
-          break;
-        }
-        default: {
-          i->inst->dump();
-          klee_error("Marking not implemented for this instruction yet!");
-        }
-        }
-      }
-    }
-  }
+  std::set<llvm::Value *> markedVariables =
+      wp->markVariables(reverseInstructionList);
 
   //TODO: Generate weakest precondition from pathCondition and/or BB instructions
   ref<Expr> expr = 0;
