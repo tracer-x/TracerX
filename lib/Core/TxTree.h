07c95039adffa7d56ae7f184acf254e899d4c8ca
rasoolmaghareh <rasool@comp.nus.edu.sg>
2017-10-19 12:32:23 +0800

    Separating the WP subsumption check from deletion subsumption check

--- lib/Core/TxTree.h
+++ lib/Core/TxTree.h
@@ -527,6 +527,13 @@ public:
   /// \brief Get the stored child WP interpolants in the parent node
   ref<Expr> getChildWPInterpolant(int flag);
 
+  /// \brief Check WP Interpolant holds at subsumption point
+  bool checkWPAtSubsumption(
+    ref<Expr> wpInterpolant, ExecutionState &state,
+	TxStore::LowerStateStore &concretelyAddressedHistoricalStore,
+    TxStore::LowerStateStore &symbolicallyAddressedHistoricalStore,
+    double timeout, int debugSubsumptionLevel);
+      
   /// \brief Copy WP to the parent node at subsumption point
   void setWPAtSubsumption(ref<Expr> _wpInterpolant);
 
