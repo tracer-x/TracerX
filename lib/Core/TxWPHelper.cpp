bb7b7cd3344d00b33e2a3cab8019fd2662dbceb9
rasoolmaghareh <rasool@comp.nus.edu.sg>
2018-10-10 15:35:30 +0800

    Adding support for Phi instruction in WP interpolation

--- lib/Core/TxWPHelper.cpp
+++ lib/Core/TxWPHelper.cpp
@@ -371,4 +371,86 @@ ref<Expr> TxWPHelper::substituteExpr(ref<Expr> base, const ref<Expr> lhs,
   return base;
 }
 
+std::set<ref<Expr> > TxWPHelper::extractVariables(ref<Expr> expr) {
+
+  std::set<ref<Expr> > set;
+
+  switch (expr->getKind()) {
+  case Expr::InvalidKind:
+  case Expr::Constant: {
+    return set;
+  }
+
+  case Expr::Read: {
+    set.insert(expr);
+    return set;
+  }
+
+  case Expr::Concat: {
+    set.insert(expr);
+    return set;
+  }
+
+  case Expr::NotOptimized:
+  case Expr::Not:
+  case Expr::Extract:
+  case Expr::ZExt:
+  case Expr::SExt: {
+    ref<Expr> kids[1];
+    kids[0] = expr->getKid(0);
+    return extractVariables(kids[0]);
+  }
+
+  case Expr::Eq:
+  case Expr::Ne:
+  case Expr::Ult:
+  case Expr::Ule:
+  case Expr::Ugt:
+  case Expr::Uge:
+  case Expr::Slt:
+  case Expr::Sle:
+  case Expr::Sgt:
+  case Expr::Sge:
+  case Expr::LastKind:
+  case Expr::Add:
+  case Expr::Sub:
+  case Expr::Mul:
+  case Expr::UDiv:
+  case Expr::SDiv:
+  case Expr::URem:
+  case Expr::SRem:
+  case Expr::And:
+  case Expr::Or:
+  case Expr::Xor:
+  case Expr::Shl:
+  case Expr::LShr:
+  case Expr::AShr: {
+    ref<Expr> kids[2];
+    kids[0] = expr->getKid(0);
+    kids[1] = expr->getKid(1);
+    std::set<ref<Expr> > set1 = extractVariables(kids[0]);
+    std::set<ref<Expr> > set2 = extractVariables(kids[1]);
+    set1.insert(set2.begin(), set2.end());
+    return set1;
+  }
+
+  case Expr::Select: {
+    ref<Expr> kids[3];
+    kids[0] = expr->getKid(0);
+    kids[1] = expr->getKid(1);
+    kids[2] = expr->getKid(2);
+    std::set<ref<Expr> > set1 = extractVariables(kids[0]);
+    std::set<ref<Expr> > set2 = extractVariables(kids[1]);
+    std::set<ref<Expr> > set3 = extractVariables(kids[2]);
+    set1.insert(set2.begin(), set2.end());
+    set1.insert(set3.begin(), set3.end());
+    return set1;
+  }
+  }
+  // Sanity check
+  klee_error("Control should not reach here in "
+             "TxWPHelper::isTargetDependent!");
+  return set;
+}
+
 } /* namespace klee */
