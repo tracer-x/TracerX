6aad0c563e76085ffef76c087c925796a5d98461
xuanlinhha <xuanlinhha@gmail.com>
2018-12-24 23:03:37 +0800

    finish this function: TxWeakestPreCondition::intersectWPExpr

--- lib/Core/TxPartitionHelper.h
+++ lib/Core/TxPartitionHelper.h
@@ -19,59 +19,22 @@
 #include "TxStore.h"
 #include "klee/Internal/Support/ErrorHandling.h"
 #include "llvm/IR/Value.h"
+#include "klee/ExecutionState.h"
 #include <iostream>
 #include <map>
 #include <string>
 
 namespace klee {
 
-template <class T> class Partition {
-public:
-  std::set<T> exprs;
-  std::set<std::string> vars;
-  void print() {
-    for (typename std::set<T>::const_iterator it = exprs.begin(),
-                                              ie = exprs.end();
-         it != ie; ++it) {
-      (*it)->dump();
-    }
-    for (std::set<std::string>::const_iterator it = vars.begin(),
-                                               ie = vars.end();
-         it != ie; ++it) {
-      llvm::outs() << (*it) << " ";
-    }
-  }
-};
-
 class TxPartitionHelper {
 public:
-  static std::vector<Partition<ref<Expr> > >
-  partitionExprsOnCond(ref<Expr> cond, std::vector<ref<Expr> > exprs);
-  static std::vector<Partition<ref<Expr> > >
-  partitionExprs(std::vector<ref<Expr> > exprs);
-
-  static std::vector<Partition<ref<TxAllocationContext> > >
-  partitionConAddStore(TxStore::TopInterpolantStore conAddStore);
-  static std::vector<Partition<ref<TxAllocationContext> > >
-  partitionConAddStoreOnCond(ref<Expr> cond,
-                             TxStore::TopInterpolantStore conAddStore);
-
-  static ref<Expr> createAnd(std::vector<ref<Expr> > exprs);
-  static ref<Expr> createAnd(std::set<ref<Expr> > exprs);
-  static void getExprsFromAndExpr(ref<Expr> e, std::vector<ref<Expr> > &exprs);
-
-  static ref<Expr> True() { return ConstantExpr::alloc(1, Expr::Bool); };
-  static ref<Expr> False() { return ConstantExpr::alloc(0, Expr::Bool); };
-
-  static void getExprVars(ref<Expr> e, std::set<std::string> &vars);
+  static std::vector<ref<Expr> > getExprsFromAndExpr(ref<Expr> e);
+  static std::set<std::string> getExprVars(ref<Expr> e);
   static bool isShared(std::set<std::string> ss1, std::set<std::string> ss2);
-
-  template <class T>
-  static std::vector<std::set<T> >
-  findConnectedComps(std::map<T, std::set<T> > graph);
-  template <class T>
-  static void visit(T node, std::set<T> &curComp,
-                    std::map<T, std::set<T> > &graph, std::set<T> &marked);
+  static std::set<std::string> diff(std::set<std::string> ss1,
+                                    std::set<std::string> ss2);
+  static ref<Expr> createAnd(std::vector<ref<Expr> > exprs);
+  static void testing(ref<Expr> expr, ExecutionState es);
 };
 }
 #endif /* TXLIB_CORE_PARTITIONHELPER_H_ */
