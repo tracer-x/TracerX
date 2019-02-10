7249e026f2f5d7cd1499f247011c7e01e37d55c2
xuanlinhha <xuanlinhha@gmail.com>
2019-02-10 20:19:40 +0800

    simplify WP expression

--- lib/Core/TxExprHelper.h
+++ lib/Core/TxExprHelper.h
@@ -28,7 +28,13 @@
 namespace klee {
 class Bound {
 public:
-  enum BType { invalid, lt, le, gt, ge };
+  enum BType {
+    invalid,
+    lt,
+    le,
+    gt,
+    ge
+  };
   ref<Expr> e;
   BType type;
   float value;
@@ -47,21 +53,24 @@ public:
   //  virtual ~TxExprHelper();
 
   static ref<Expr> simplifyNot(ref<Expr> e);
-  //  static ref<Expr> simplifyLinear(ref<Expr> e);
-  //  static bool extractCoeff(ref<Expr> e, int mul,
-  //                           std::map<ref<Expr>, int> &ref2coeff);
-  //  static ref<Expr> makeExpr(ref<Expr> e, std::map<ref<Expr>, int>
-  //  &ref2coeff);
 
-  //  static ref<Expr> rangSimplify(ref<Expr> e);
-  //  static std::vector<ref<Expr> >
-  //  rangeSimplifyFromExprs(std::vector<ref<Expr> > exprs);
-  //  static std::vector<ref<Expr> >
-  //  rangeSimplifyFromExprs(std::set<ref<Expr> > exprs);
-  //  static std::vector<ref<Expr> >
-  //  combineSharedSingleVarExprs(std::vector<ref<Expr> > exprs);
-  //  static Bound getBound(ref<Expr> expr);
-  //  static bool isaVar(ref<Expr> e);
+  // If input is linear, combine coefficient of same variable togethers
+  // eg: 2X + 4X + 3Y - 2 < 8 is siplied to
+  // 6X + 3Y < 10
+  static ref<Expr> simplifyLinear(ref<Expr> e);
+  static bool extractCoeff(ref<Expr> e, int mul,
+                           std::map<ref<Expr>, int> &ref2coeff);
+  static ref<Expr> makeExpr(ref<Expr> e, std::map<ref<Expr>, int> &ref2coeff);
+
+  static ref<Expr> rangSimplify(ref<Expr> e);
+  static std::vector<ref<Expr> >
+  rangeSimplifyFromExprs(std::vector<ref<Expr> > exprs);
+  static std::vector<ref<Expr> >
+  rangeSimplifyFromExprs(std::set<ref<Expr> > exprs);
+  static std::vector<ref<Expr> >
+  combineSharedSingleVarExprs(std::vector<ref<Expr> > exprs);
+  static Bound getBound(ref<Expr> expr);
+  static bool isaVar(ref<Expr> e);
   //  static bool isFalse(ref<Expr> e);
 
   //  static ref<Expr> removeShadowExprs(ref<Expr> e,
