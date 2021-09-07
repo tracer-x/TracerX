96760b93dca2a07dacbc1110d232cb19e4777dd6
Arpita <arpitad10j@gmail.com>
2021-09-07 11:14:35 +0800

    add updated files

--- include/klee/Config/CompileTimeInfo.h
+++ include/klee/Config/CompileTimeInfo.h
@@ -1,3 +1,3 @@
 #define KLEE_BUILD_MODE "Release+Asserts"
-#define KLEE_BUILD_REVISION "1afe28ce0ae22aeb5e12c3d42a56f5421c704e30"
-#define KLEE_BUILD_TAG "v1.0.0-1878-g1afe28c"
+#define KLEE_BUILD_REVISION "c2a594941e82cbcaf052dcf4b2f62309de47aaeb"
+#define KLEE_BUILD_TAG "v1.0.0-1879-gc2a5949"
