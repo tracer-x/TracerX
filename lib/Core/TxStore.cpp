f3446cc60868bcbff6409325ad115f92d81048f3
Arpita Dutta <arpitad10j@gmail.com>
2022-10-11 17:42:55 +0800

    Checking the historical store

--- lib/Core/TxStore.cpp
+++ lib/Core/TxStore.cpp
@@ -297,10 +297,6 @@ ref<TxAllocationContext>
 TxStore::getAddressofLatestCopyLLVMValue(llvm::Value *val) {
   ref<TxAllocationContext> address;
   bool foundValue = false;
-  //llvm::outs()<<"getAddressofLatestCopyLLVMValue:::::::::::n";
-  //llvm::outs()<<val->Name;
-  //val->dump();
-
   for (TopStateStore::const_iterator it = internalStore.begin(),
                                      ie = internalStore.end();
        it != ie; ++it) {
@@ -316,50 +312,6 @@ TxStore::getAddressofLatestCopyLLVMValue(llvm::Value *val) {
       }
     }
   }
-//  llvm::outs()<<"lets\n";
-////address->dump();
-//llvm::outs()<<"lets\n";
-  // Maybe NULL
-//---- 18/02/2022----
-//if(address==NULL){
-//	 for (LowerStateStore::const_iterator
-//	           it = concretelyAddressedHistoricalStore.begin(),
-//	           ie = concretelyAddressedHistoricalStore.end();
-//	       it != ie; ++it) {
-////
-//		 ref<TxAllocationContext> temp = (*it).second;
-////		     //llvm::outs()<<"Address-01\n";
-//		     if (temp->getValue() == val) {
-////		       if (!foundValue) {
-//		         address = temp;
-////		         foundValue = true;
-////		       }
-//	     }
-//	  }
-////
-//}
-
-//if(address==NULL){
-//	 for (LowerStateStore::const_iterator
-//	           it = symbolicallyAddressedHistoricalStore.begin(),
-//	           ie = symbolicallyAddressedHistoricalStore.end();
-//	       it != ie; ++it) {
-//
-//		 ref<TxAllocationContext> temp = (*it).first;
-//		     //llvm::outs()<<"Address-01\n";
-//		     if (temp->getValue() == val) {
-//		       if (!foundValue) {
-//		         address = temp;
-//		         foundValue = true;
-//		       } else if (temp->getCallHistory().size() >
-//		                  address->getCallHistory().size()) {
-//		         address = temp;
-//		       }
-//		     }
-//	  }
-//
-//}
-//------------------------
   return address;
 }
 
@@ -370,15 +322,25 @@ for (LowerStateStore::const_iterator
           it = symbolicallyAddressedHistoricalStore.begin(),
           ie = symbolicallyAddressedHistoricalStore.end();
       it != ie; ++it) {
-
 ref<TxStoreEntry> temp = (*it).second;
-    //llvm::outs()<<"Address-01\n";
-    if (temp->getValue() == val) {
-      if (!foundValue) {
+    if (temp->getValue() == val) {     
         address = temp;
         foundValue = true;
-      }
     }
+}
+if(!foundValue)
+{
+for (LowerStateStore::const_iterator
+          it = concretelyAddressedHistoricalStore.begin(),
+          ie = concretelyAddressedHistoricalStore.end();
+      it != ie; ++it) {
+ref<TxStoreEntry> temp = (*it).second;
+    if (temp->getValue() == val) {     
+        address = temp;
+        foundValue = true;
+    }
+}
+
 }
 return address;
 }
